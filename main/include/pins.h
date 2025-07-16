e1490d75caa563f421e1627a4444f06f46ef3c65
IceCreamSandwhich <isaacosei4583@gmail.com>
2025-07-16 17:00:28 -0400

    Updated Pins.h. Changed wireless driving website back to a header file. Started button logic for wireless driving website

--- main/include/pins.h
+++ main/include/pins.h
@@ -1,25 +1,33 @@
 #pragma once
 #include "driver/gpio.h"
 
-#ifdef SEED_S3
+#ifdef ESP32S3_ZERO
 
-#define D0 GPIO_NUM_1
-#define D1 GPIO_NUM_2
-#define D2 GPIO_NUM_3
-#define D3 GPIO_NUM_4
-#define D4 GPIO_NUM_5
-#define D5 GPIO_NUM_6
-#define D6 NULL // Connected to U0TXD
-#define D7 NULL // Connected to U0RXD
-#define D8 GPIO_NUM_7
-#define D9 GPIO_NUM_7
-#define D10 GPIO_NUM_9
+// Digital Pins
+#define D0 GPIO_NUM_0   // Boot button (use carefully)
+#define D1 GPIO_NUM_1   // TXD0 (Serial)
+#define D2 GPIO_NUM_2   // RXD0 (Serial)
+#define D3 GPIO_NUM_3   // SPI CS
+#define D4 GPIO_NUM_4   // SPI SCK
+#define D5 GPIO_NUM_5   // SPI MOSI
+#define D6 GPIO_NUM_6   // SPI MISO
+#define D7 GPIO_NUM_7   // Not broken out
+#define D8 GPIO_NUM_8   // Not broken out
+#define D9 GPIO_NUM_9   // Not broken out
+#define D10 GPIO_NUM_10 // Not broken out
 
-#define A0 D0
-#define A1 D1
-#define A2 D2
-#define A3 D3
-#define A9 D9
-#define A10 D10
+// Analog Pins (ADC1 channels)
+#define A0 GPIO_NUM_1   // ADC1_CH0 (shared with D1/TXD0)
+#define A1 GPIO_NUM_2   // ADC1_CH1 (shared with D2/RXD0)
+#define A2 GPIO_NUM_3   // ADC1_CH2 (shared with D3)
+#define A3 GPIO_NUM_4   // ADC1_CH3 (shared with D4)
 
-#endif
+// Special Function Pins
+#define LED_BUILTIN GPIO_NUM_13  // Onboard LED
+#define BOOT_BUTTON GPIO_NUM_0   // Boot button
+
+// Warning: Pins used for onboard functions
+#define USB_DM GPIO_NUM_19  // USB D- (don't use)
+#define USB_DP GPIO_NUM_20  // USB D+ (don't use)
+
+#endif // ESP32S3_ZERO
